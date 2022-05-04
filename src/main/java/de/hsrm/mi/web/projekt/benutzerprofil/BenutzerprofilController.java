b09fcaf727a10a82f11beab53a20b8633c63d346
mbach001 <marc.bachmann@student.hs-rm.de>
2022-05-04 16:37:50 +0200

    Aufgabe 3 und 4

--- src/main/java/de/hsrm/mi/web/projekt/benutzerprofil/BenutzerprofilController.java
+++ src/main/java/de/hsrm/mi/web/projekt/benutzerprofil/BenutzerprofilController.java
@@ -6,15 +6,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class BenutzerprofilController {
 
-    @GetMapping("/benutzerprofil")
-    @ModelAttribute
-    public String benutzerprofil_get(Model m) {
+    Logger logger = LoggerFactory.getLogger(BenutzerprofilController.class);
+    
+
+    @ModelAttribute("profil")
+    public void initProfil(Model m){
         BenutzerProfil profil = new  BenutzerProfil();
         profil.setName("Marc");
         profil.setGeburtsdatum(LocalDate.of(1,1,1));
@@ -23,13 +27,18 @@ public class BenutzerprofilController {
         profil.setLieblingsfarbe("Blau");
         profil.setInteressen("Programmieren, Schlafen");
 
-
+        m.addAttribute("profil", profil);
         m.addAttribute("name", profil.getName());
         m.addAttribute("geburtsdatum", profil.getGeburtsdatum());
         m.addAttribute("adresse", profil.getAdresse());
         m.addAttribute("email", profil.getEmail());
-        m.addAttribute("lieblingsbarben", profil.getLieblingsfarbe());
+        m.addAttribute("lieblingsfarbe", profil.getLieblingsfarbe());
         m.addAttribute("interessen", profil.getInteressenListe(profil.getInteressen()));
+    }
+
+    @GetMapping("/benutzerprofil")
+    public String benutzerprofil_get() {
+        
         return "/benutzerprofil/profilansicht";
     }
 
