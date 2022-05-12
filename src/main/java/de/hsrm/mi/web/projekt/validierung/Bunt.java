fa5feeceeb7f9aed2dc858b67ecef85f8cfb8d75
mbach001 <marc.bachmann@student.hs-rm.de>
2022-05-12 15:21:53 +0200

    VSC geht nicht

--- src/main/java/de/hsrm/mi/web/projekt/validierung/Bunt.java
+++ src/main/java/de/hsrm/mi/web/projekt/validierung/Bunt.java
@@ -1,9 +1,13 @@
-@Target({ElementType.FIELD})
-@Rentation(RentationPolicy.RUNTIME)
+package de.hsrm.mi.web.projekt.validierung;
+
+@Target({ ElementType.FIELD })
+@Retention(RetentionPolicy.RUNTIME)
 @Constraint(validatedBy=BuntValidator.class)
 
 @Documented
 public @interface Bunt {
     String message() default "Wert ist nicht Bunt genug";
 
+    //String value();
+    
 }
--- src/main/java/de/hsrm/mi/web/projekt/validierung/BuntValidator.java
+++ src/main/java/de/hsrm/mi/web/projekt/validierung/BuntValidator.java
@@ -1,34 +1,44 @@
-package de.mi.hsrm.web.project.validierung;
+package de.hsrm.mi.web.projekt.validierung;
 
-public class BuntValidator implements ConstraintValidator<Bunt>{
+public class BuntValidator implements ConstraintValidator<Bunt, String>{
     
+    //protected String color;
     @Override
-    public boolean isValid(String lieblingsfarbe, ConstraintValidatorContext ctx) {
-        if(lieblingsfarbe.isEmpty()){
-            return false;
-        }
-
-        if(lieblingsfarbe == null || lieblingsfarbe.length == 0){
+    public boolean isValid(String farbe, ConstraintValidatorContext ctx) {
+        if(farbe == null || farbe.length() == 0 || farbe.isEmpty()){
             return true;
         }
 
-        // erstes zeichen # && (länge = 4 || länge = 6 
-        if("#".equals(lieblingsfarbe.charAt(0)) && (lieblingsfarbe.length() == 4 || lieblingsfarbe.length() == 6)){
-            if(lieblingsfarbe.length() == 4){
-                //wertepaare überprüfen
-                if(lieblingsfarbe.charAt(1) != lieblingsfarbe.charAt(2) && lieblingsfarbe.charAt(1) != lieblingsfarbe.charAt(3) && lieblingsfarbe.charAt(2) != lieblingsfarbe.charAt(3)){
-                    return true;
-                }else {
-                    return false;
-                }
-            }else {
-                if(!lieblingsfarbe.substring(1,2).equals(lieblingsfarbe.substring(3,4)) && !lieblingsfarbe.substring(1, 2).equals(lieblingsfarbe.substring(5, 6)) && lieblingsfarbe.substring(3, 4).equals(lieblingsfarbe.substring(5, 6))){
-                    return true;
-                }else {
-                    return false;
-                }
+        //erstes Zeichen #
+        if((farbe.charAt(0) != '#')){
+            return false;
+            //return "erstes Zeichen kein #";
+        }
+
+        //drei oder 6 Ziffern nach #
+        if(!(farbe.length() == 4 || farbe.length() == 7)){
+            return false;
+            //return "Farbe nicht 4 oder 7 stellen Lang";
+        }
+
+        //gültige hex Zahlen
+        if(!(farbe.substring(1, farbe.length()).matches("-?[0-9a-fA-F]+"))){
+            return false;
+            //return "Farbe enthält ungültige Ziffern";
+        }
+
+        //farbe bunt genug
+        if(farbe.length() == 4){
+            if((farbe.charAt(1) == farbe.charAt(2)) || (farbe.charAt(1) == farbe.charAt(3)) || (farbe.charAt(2) == farbe.charAt(3))){
+                return false;
+                //return "Die Farbe ist nicht Bunt genug";
             }
-        } 
-        return false;
+        }else{
+            if((farbe.substring(1, 3).equals(farbe.substring(3, 5))) || (farbe.substring(1, 3).equals(farbe.substring(5, 7))) || (farbe.substring(3, 5).equals(farbe.substring(5, 7)))) {
+                return false;
+                //return "Die Farbe ist nicht Bunt genug";
+            }
+        }
+        return true;
     }
 }
