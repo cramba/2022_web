aefdbe0a5f9f71182bd90ef32769420c4a3fb834
mbach001 <marc.bachmann@student.hs-rm.de>
2022-05-22 09:02:43 +0200

    Blatt 5 Aufgabe 1 - 3 mittel

--- src/main/java/de/hsrm/mi/web/projekt/benutzerprofil/BenutzerprofilRepository.java
+++ src/main/java/de/hsrm/mi/web/projekt/benutzerprofil/BenutzerprofilRepository.java
@@ -1,7 +1,8 @@
 package de.hsrm.mi.web.projekt.benutzerprofil;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
+@Repository
 public interface BenutzerprofilRepository extends JpaRepository <BenutzerProfil, Long> {
 
     
